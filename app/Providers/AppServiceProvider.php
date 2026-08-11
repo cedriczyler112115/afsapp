5d821599da94f5b74b87c62f5e85de609f3481b3
cedriczyler <abgonzales.focrg@dswd.gov.ph>
2026-08-11 11:00:58 +0800

    fixedgooglel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+
+            $appUrl = rtrim((string) config('app.url'), '/');
+            if ($appUrl !== '') {
+                URL::forceRootUrl($appUrl);
+            }
+        }
     }
 }
